9212c30f7c4c52d345943419029d8a20f5c3c7ea
Jean BILLAUD <billau_j@etna-alternance.net>
2017-11-30 09:09:36 +0100

    On peut copier le fichier, pour le moment il se copie dans le dossier download

--- app/src/main/java/com/etna/mob3/mob3/MainActivity.kt
+++ app/src/main/java/com/etna/mob3/mob3/MainActivity.kt
@@ -1,32 +1,36 @@
 package com.etna.mob3.mob3
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.support.design.widget.Snackbar
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.net.URISyntaxException
 import java.io.File
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
 
     private val READ_REQUEST_CODE = 42
+    private val FILE_SELECT_CODE = 1
+    private val TAG = "MainActivity"
+    private val APP_DIR: String = "/storage/emulated/0/Download/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         downloadButton.setOnClickListener {
             downloadButtonPressed()
@@ -36,7 +40,7 @@ class MainActivity : AppCompatActivity() {
     fun downloadButtonPressed() {
         Log.d("download","download button pressed")
 
-        performFileSearch()
+        showFileChooser()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,50 +59,57 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun performFileSearch() {
+    fun showFileChooser() {
 
-        // The wanted action (we want to open a document)
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
         intent.addCategory(Intent.CATEGORY_OPENABLE)
 
-        // Filter to show only images, using the image MIME data type.
-        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
-        // To search for all documents available via installed storage providers,
-        // it would be "*/*".
-        intent.type = "*/*"
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE)
+        } catch (ex: android.content.ActivityNotFoundException) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show()
+        }
 
-        startActivityForResult(intent, READ_REQUEST_CODE)
 
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int,
-                                         resultData: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        val dl_file: File
+        val cp_file: File
 
-        // Document is selected
-        Log.d("result activity", "on result")
-
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-
-            var uri: Uri? = null
-
-            if (resultData != null) {
-
-                // the uri of the selected document
-                resultData.data
-                uri = resultData.data
-
-                val selectedfile = File(uri.path)
-                val fileCopy: File = File(uri.path)
-
-                var downloader = Downloader()
-
-                Downloader()
-               // downloader.copyFile(selectedfile, fileCopy)
-
-                Log.d("", "Uri: " + uri!!.toString())
+        when (requestCode) {
+            FILE_SELECT_CODE -> if (resultCode == Activity.RESULT_OK) {
+                // Get the Uri of the selected file
+                val uri = data.data
+                Log.d(TAG, "File Uri: " + uri!!.toString())
+                // Get the path in file utils
+                val path = uri.path
+                Log.d(TAG, "File Path: " + path)
+                dl_file = File("/storage/emulated/0/Download/1_ENHANCED_25.his")
+                Log.d(TAG, "file name: " + dl_file.absolutePath)
+                cp_file = File(APP_DIR + "TESTSAMERE.his")
+                val toto = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
+                Log.d("test", toto)
+//                try {
+//                    val toto = dl_file.readLines(Charsets.UTF_8)
+//                    Log.d(TAG, toto[1])
+//                } catch (e: Exception) {
+//                    Log.d(TAG, e.toString())
+//                }
+                try {
+                    dl_file.copyTo(cp_file)
+                    Log.d("Result", "files copied")
+                } catch (e: Exception) {
+                    Log.d(TAG, e.toString())
+                }
             }
         }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
