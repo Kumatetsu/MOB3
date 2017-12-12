441a5730ec42d6b55c6d7674da1f623b8db10afa
Jean BILLAUD <billau_j@etna-alternance.net>
2017-12-12 17:51:07 +0100

    Parsing en place, plus qu'a repeter pour tout le reste.

--- app/src/main/java/com/etna/mob3/mob3/tools/parser.kt
+++ app/src/main/java/com/etna/mob3/mob3/tools/parser.kt
@@ -25,6 +25,33 @@ object Tools {
 
     fun parseFile (file: File): String {
         val datas = FileDatas()
+        var file_infos = file.readLines()
+        var air_temp = ArrayList<Float>()
+
+        file_infos = file_infos.drop(2)
+
+        file_infos.forEach {
+            var parsedLine : List<String> = it.split("\t")
+
+            if ("" != parsedLine[33].trim()) {
+                air_temp.add(parsedLine[33].toFloat())
+            } else {
+                air_temp.add("0".toFloat())
+            }
+
+
+        }
+
+        Log.d("air_temp length", air_temp.size.toString())
+        Log.d("air_temp value", air_temp.toString())
+
+//        file.forEachLine {
+//            parsed_file.plus(it.split("\t"))
+//        }
+
+//        var line = file_infos[2]
+//        Log.d("split", line.split("\t").toString())
+
         return "OK"
     }
 }
