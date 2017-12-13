31c1df8c10f0f682aa8b2ad0b92147f66311585f
Jeremy91890 <jeremy.debelleix@gmail.com>
2017-12-13 15:42:57 +0100

    create header xml

--- app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
+++ app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
@@ -3,8 +3,11 @@ package com.etna.mob3.mob3
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TableRow
+import android.widget.TextView
 import com.etna.mob3.mob3.classes.FileDatas
 import com.etna.mob3.mob3.tools.Tools
+import kotlinx.android.synthetic.main.activity_meteo_info.*
 import java.io.File
 
 class MeteoInfoActivity : AppCompatActivity() {
@@ -15,13 +18,22 @@ class MeteoInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meteo_info)
 
+      //this.air_temperature.setBackgroundColor(Color.BLACK)
+
+        val heading = TableRow(this)
+        var analyseLabel = TextView(this)
+        analyseLabel.text = "Analyse"
+        heading.addView(analyseLabel)
+
+        this.data_table.addView(heading)
+
         if (intent != null) {
             if (intent.extras != null) {
                 var file: File = intent.extras.get("file") as File
 
-                var fileData: FileDatas = Tools.parseFile(file)
+                var fileData = Tools.parseFile(file)
 
-                Log.d("filedata", "" + fileData.air_hour_max)
+                Log.d("filedata", "" + fileData)
             }
         }
     }
