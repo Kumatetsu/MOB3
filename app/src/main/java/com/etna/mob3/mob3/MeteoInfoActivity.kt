b6c2e1d6225fc5275d39d7c7df62f14ce2127cfe
Jeremy91890 <jeremy.debelleix@gmail.com>
2017-12-13 15:58:03 +0100

    load air temperature dynamically

--- app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
+++ app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
@@ -2,7 +2,6 @@ package com.etna.mob3.mob3
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.TableRow
 import android.widget.TextView
 import com.etna.mob3.mob3.classes.FileDatas
@@ -12,29 +11,65 @@ import java.io.File
 
 class MeteoInfoActivity : AppCompatActivity() {
 
-    private val fileData: FileDatas? = null
+    private var fileData: FileDatas? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meteo_info)
 
-      //this.air_temperature.setBackgroundColor(Color.BLACK)
-
-        val heading = TableRow(this)
-        var analyseLabel = TextView(this)
-        analyseLabel.text = "Analyse"
-        heading.addView(analyseLabel)
-
-        this.data_table.addView(heading)
-
         if (intent != null) {
             if (intent.extras != null) {
                 var file: File = intent.extras.get("file") as File
 
-                var fileData = Tools.parseFile(file)
+                this.fileData = Tools.parseFile(file)
 
-                Log.d("filedata", "" + fileData)
+                fillDataTable()
             }
         }
     }
+
+    private fun fillDataTable() {
+
+        load_Air_Temperature()
+       /* load_Rel_Humidity()
+        load_Air_Pressure()
+        load_Local_WS_2min_mnm()*/
+
+    }
+
+    private fun load_Air_Temperature() {
+
+        val air_temperature = TableRow(this)
+
+        var title = TextView(this)
+        var min = TextView(this)
+        var hour_min = TextView(this)
+        var avg_day = TextView(this)
+        var max = TextView(this)
+
+        title.text = "AIR_TEMPERATURE"
+        min.text = this.fileData!!.air_temp_min.toString()
+        hour_min.text = this.fileData!!.air_hour_min
+        avg_day.text = this.fileData!!.air_temp_avg.toString()
+        max.text = this.fileData!!.air_temp_max.toString()
+
+        air_temperature.addView(title)
+        air_temperature.addView(min)
+        air_temperature.addView(hour_min)
+        air_temperature.addView(avg_day)
+        air_temperature.addView(max)
+
+        this.data_table.addView(air_temperature)
+    }
+
+    private fun load_Rel_Humidity() {
+
+    }
+    private fun load_Air_Pressure() {
+
+    }
+    private fun load_Local_WS_2min_mnm() {
+
+    }
+
 }
