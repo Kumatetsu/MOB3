21f48b4d712bdd1d5b9b38a664679db5e8863331
Jeremy91890 <jeremy.debelleix@gmail.com>
2017-12-13 11:14:23 +0100

    parse du fichier choisi dans meteoInfoActivity

--- app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
+++ app/src/main/java/com/etna/mob3/mob3/MeteoInfoActivity.kt
@@ -1,12 +1,28 @@
 package com.etna.mob3.mob3
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.etna.mob3.mob3.classes.FileDatas
+import com.etna.mob3.mob3.tools.Tools
+import java.io.File
 
 class MeteoInfoActivity : AppCompatActivity() {
+
+    private val fileData: FileDatas? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meteo_info)
+
+        if (intent != null) {
+            if (intent.extras != null) {
+                var file: File = intent.extras.get("file") as File
+
+                var fileData: FileDatas = Tools.parseFile(file)
+
+                Log.d("filedata", "" + fileData.air_hour_max)
+            }
+        }
     }
 }
