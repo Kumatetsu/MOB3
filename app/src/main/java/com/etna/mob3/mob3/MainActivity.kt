a583c10483fdf7df57bf593a3a8c3dd138b6d78c
Jeremy91890 <jeremy.debelleix@gmail.com>
2017-12-14 11:33:32 +0100

    remove the useless toolbar

--- app/src/main/java/com/etna/mob3/mob3/MainActivity.kt
+++ app/src/main/java/com/etna/mob3/mob3/MainActivity.kt
@@ -14,7 +14,6 @@ import com.etna.mob3.mob3.classes.CustomAdapter
 import com.etna.mob3.mob3.classes.DataModel
 import com.etna.mob3.mob3.classes.FileDatas
 import com.etna.mob3.mob3.tools.Tools
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
 
@@ -31,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+       // setSupportActionBar(toolbar)
 
         fillList()
 
